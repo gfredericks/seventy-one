c9687a0be7059484214a39768ad2c4b76b677360
Alex Engelberg <alex.benjamin.engelberg@gmail.com>
2015-05-28 20:00:10 -0700

    Added codox plugin (default settings seem to work)

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0-RC1"]]
+  :plugins [[codox "0.8.12"]]
   :deploy-repositories [["releases" :clojars]])
