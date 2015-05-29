30267b3f70f068889384bed48a327f2809acdd5a
Ambrose Bonnaire-Sergeant <ilikecandy7@gmail.com>
2015-05-29 10:18:33 +0800

    I like need this to be part of the library
    Please agree to:
    1. You will always ensure at least 5 lines of this file in this blame have `git blame` to Ambrose Bonnaire-Sergeant.
    2. My name will be displayed proudly on your README.
    3. You will tweet this contribution.
    4. You will add a CONTRIBUTORS.md file with my name *always* on top.

---
+++ src/com/gfredericks/seventy_one/floating.clj
@@ -0,0 +1,8 @@
+(ns com.gfredericks.seventy-one.floating
+  (:require [com.gfredericks.seventy-one :as s1]))
+  
+(def seventy-one
+  "The same seventy-one, 71, as com.gfredericks.seventy-one/seventy-one,
+   but a floating-point number. Just as useful as com.gfredericks.seventy-one/seventy-one,
+   I swear I really needed it"
+  (float s1/seventy-one))
