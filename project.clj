f4081d6a2480c2cd23eae044b94f680d9adb5e6c
Tony van Riet <tonyvanriet@gmail.com>
2015-05-29 22:55:30 -0500

    Increase test coverage

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "https://github.com/gfredericks/seventy-one"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-RC1"]]
+  :dependencies [[org.clojure/clojure "1.7.0-RC1"]
+                 [org.clojure/test.check "0.7.0"]]
   :plugins [[codox "0.8.12"]]
   :deploy-repositories [["releases" :clojars]])
