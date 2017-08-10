22f38b887ed45f815ac9521f1b155788b040329d
Gary Fredericks <fredericksgary@gmail.com>
2017-08-10 06:20:45 -0500

    Version 0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.gfredericks/seventy-one "0.1.2-SNAPSHOT"
+(defproject com.gfredericks/seventy-one "0.1.2"
   :description "71 in Clojure"
   :url "https://github.com/gfredericks/seventy-one"
   :license {:name "Eclipse Public License"
