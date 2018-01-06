92e1a49ea97cc5f6a29edfda036eca66c844ece9
Alex Engelberg <alex.benjamin.engelberg@gmail.com>
2018-01-06 00:20:45 -0800

    Version bumps

--- project.clj
+++ project.clj
@@ -3,12 +3,11 @@
   :url "https://github.com/gfredericks/seventy-one"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]]
   :plugins [[codox "0.8.12"]
             [lein-cljsbuild "1.1.7"]]
   :deploy-repositories [["releases" :clojars]]
-  :profiles {:dev {:dependencies [[org.clojure/clojurescript "1.9.854"]
-                                  [org.clojure/spec.alpha "0.1.123"]
+  :profiles {:dev {:dependencies [[org.clojure/clojurescript "1.9.946"]
                                   [org.clojure/test.check "0.10.0-alpha2"]]
                    :cljsbuild {:builds [{:source-paths ["src" "test"]
                                          :compiler {:output-to "resources/private/js/unit-test.js"
